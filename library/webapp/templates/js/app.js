cd7784fe4d49f54214ed6b27ccb23afaea320d01
Danny <pengdanny23@gmail.com>
2015-09-12 09:50:05 -0700

    Set up angular routing

--- library/webapp/templates/js/app.js
+++ library/webapp/templates/js/app.js
@@ -2,3 +2,20 @@ var renisonlibraryApp = angular.module('renisonlibraryApp', [
   'ngRoute',
   'mainControllers'
 ]);
+
+renisonlibraryApp.config(function($routeProvider) {
+    $routeProvider
+
+        // route for the home page
+        .when('/', {
+            templateUrl : 'pages/home.html',
+            controller  : 'homeController'
+        })
+
+        // route for the search page
+        .when('/search', {
+            templateUrl : 'pages/search.html',
+            controller  : 'searchController'
+        })
+
+});
--- library/webapp/templates/js/controller.js
+++ library/webapp/templates/js/controller.js
@@ -2,5 +2,10 @@ var mainControllers = angular.module('mainControllers', []);
 
 mainControllers.controller('homeController', ['$scope',
     function($scope) {
-        $scope.message = 'Hello World';
+        $scope.message = 'This is home page';
+    }]);
+
+mainControllers.controller('searchController', ['$scope',
+    function($scope) {
+        $scope.message = 'This is search page';
     }]);
