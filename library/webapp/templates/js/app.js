26b8049882a73d113a57d9e2c9c602470135c32b
rainybreeze777 <psting3456@gmail.com>
2015-09-21 23:37:46 -0400

    Implement frontend using AngularJS. The most up-to-date frontend code can be found at https://github.com/19506jk/library-pages.git

--- library/webapp/templates/js/app.js
+++ library/webapp/templates/js/app.js
@@ -1,13 +1,15 @@
 var renisonlibraryApp = angular.module('renisonlibraryApp', [
   'ngRoute',
-  'mainControllers'
+  'mainControllers',
+  'searchController',
+  'bookDetailsController'
 ]);
 
 renisonlibraryApp.config(function($routeProvider) {
     $routeProvider
 
         // route for the home page
-        .when('/', {
+        .when('/home', {
             templateUrl : 'pages/home.html',
             controller  : 'homeController'
         })
@@ -15,7 +17,38 @@ renisonlibraryApp.config(function($routeProvider) {
         // route for the search page
         .when('/search', {
             templateUrl : 'pages/search.html',
-            controller  : 'searchController'
+            controller  : 'searchController',
+            reloadOnSearch : false
         })
 
+        // route for the book details page
+        .when('/detail/:bookId', {
+            templateUrl : 'pages/book_detail.html',
+            controller  : 'bookDetailsController'
+        })
+
+});
+
+//Creates global functions
+//Controllers can directly call these functions
+//To modify globally available variables
+renisonlibraryApp.run(function($rootScope){
+	$rootScope.changeActiveTab = function(switchToTab){
+		$rootScope.home_active_status = '';
+		$rootScope.booklist_active_status = '';
+		$rootScope.search_active_status = '';
+		$rootScope.about_active_status = '';
+		$rootScope.contact_active_status = '';
+		if (switchToTab === HOME_TAB) {
+			$rootScope.home_active_status = 'active';
+		} else if (switchToTab === BOOKLIST_TAB) {
+			$rootScope.booklist_active_status = 'active';
+		} else if (switchToTab === SEARCH_TAB) {
+			$rootScope.search_active_status = 'active';
+		} else if (switchToTab === ABOUT_TAB) {
+			$rootScope.about_active_status = 'active';
+		} else if (switchToTab === CONTACT_TAB) {
+			$rootScope.contact_active_status = 'active';
+		}
+	}
 });
